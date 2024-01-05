258fdb3cdb88b64eb23ce500b9214f6866cb372e
Aman <amangupta3579@gmail.com>
2024-01-05 11:47:49 +0530

    chat to int

--- input.cpp
+++ input.cpp
@@ -19,7 +19,7 @@ int main(){
      int d=66;
     b=d;  //since b is char type so it will store 66 as B.
     cout<<b;
-    //when we insert interger into character datatype, integer is 32 bits and char is 4 bits. So, there is higher chance of data loss as 32 bits are going to be unfit into 4 bits.
+    ////when we insert interger into character datatype, integer is 32 bits and char is 4 bits. So, there is higher chance of data loss as 32 bits are going to be unfit into 4 bits.
      //only last 8 bits of integer is fitted into char of 8 bits.
     // favourable condition: bool->char->int->double
 }
