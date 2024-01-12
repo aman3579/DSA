adcb4be49f49afe9a5439aa904721682abc8daad
Aman <amangupta3579@gmail.com>
2024-01-12 23:35:10 +0530

    triangle

---
+++ switch.cpp
@@ -0,0 +1,59 @@
+// // //switch
+// // #include<iostream>
+// // using namespace std;
+// // int main(){
+// //     int num, i;
+// //     cout<<"Enter the case: ";
+// //     cin>>num;
+// //     switch (num)
+// //     {
+// //     case 1:
+// //     cout<<"1 is entered.";
+// //         break;
+// //     case 2:
+// //     cout<<"2 is eneterd.";
+// //         break;
+// //     case 3:
+// //     cout<<"3 is enetered";
+// //         break;
+// //     case 4:
+// //     cout<<"4 is enetered.";
+// //         break;
+// //     case 5:
+// //     cout<<"5 is enetered.";
+// //         break;
+// //     default:
+// //         break;
+// //     }
+// // }
+
+// //switch
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char num;
+//     cout<<"Enter the case: ";
+//     cin>>num;
+//     switch (num)
+//     {
+//     case 'a':
+//     cout<<"1 is entered.";
+//         break;
+//     case 'b':
+//     cout<<"2 is eneterd.";
+        
+//     case 'c':
+//     cout<<"3 is enetered";
+        
+//     case 'd':
+//     cout<<"4 is enetered.";
+//         break;
+//     case 'e':
+//     cout<<"5 is enetered.";
+//         break;
+//     default:
+//         break;
+//     }
+// }
+
+//Scope of a variable
